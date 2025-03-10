6506c36a6b9a1df36745e7f099a6205620944dca
sugralld <shafaldabiilqis@gmail.com>
2025-03-10 22:52:35 +0700

    Added cart features

---
+++ .history/src/components/layout/horizontal/FooterContent_20250310152648.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+// Third-party Imports
+import classnames from 'classnames'
+
+// Util Imports
+import { horizontalLayoutClasses } from '@layouts/utils/layoutClasses'
+
+const FooterContent = () => {
+  return (
+    <div
+      className={classnames(horizontalLayoutClasses.footerContent, 'flex items-center justify-between flex-wrap gap-4')}
+    >
+      <p>
+        <span className='text-textSecondary'>{`© ${new Date().getFullYear()} `}</span>
+        <span className='text-textSecondary'>Kalbe Devstream UI</span>
+      </p>
+    </div>
+  )
+}
+
+export default FooterContent
