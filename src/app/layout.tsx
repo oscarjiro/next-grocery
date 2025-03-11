f9eaaed0a0fa122db8a4df8d1b730797fda8b375
oscarjiro <oscarjiroj@gmail.com>
2025-03-12 05:46:54 +0700

    feat: add favicons and web manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,15 @@ import '@assets/iconify-icons/generated-icons.css'
 
 export const metadata = {
   title: 'Next Grocery',
-  description: 'Next Grocery'
+  description: 'Next Grocery',
+  icons: {
+    icon: [
+      { rel: 'icon', url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { rel: 'icon', url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' }
+    ],
+    apple: { url: '/apple-touch-icon.png', sizes: '180x180' }
+  },
+  manifest: '/site.webmanifest'
 }
 
 const RootLayout = async (props: ChildrenType) => {
